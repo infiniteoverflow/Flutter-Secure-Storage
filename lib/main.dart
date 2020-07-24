35aa9bdc11747fb39f621c84a7acdf6b8748facd
infiniteoverflow <aswingopinathan1871@gmail.com>
2020-07-24 21:13:48 +0530

    Added the required dependencies and code

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,19 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -34,84 +24,180 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+enum _Actions { deleteAll }
+enum _ItemActions { delete, edit }
 
-  void _incrementCounter() {
+class _MyHomePageState extends State<MyHomePage> {
+
+  final _storage = FlutterSecureStorage();
+
+  List<_SecItem> _items = [];
+
+  @override
+  void initState() { 
+    super.initState();
+    _readAll();
+  }
+
+  // A Method to read all data in the storage and to display as a list
+  Future<Null> _readAll() async {
+    final all = await _storage.readAll();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      return _items = all.keys
+          .map((key) => _SecItem(key, all[key]))
+          .toList(growable: false);
     });
   }
 
+// Delete all entries in the storage
+  void _deleteAll() async {
+    await _storage.deleteAll();
+    _readAll();
+  }
+
+  //Add New Item
+  void _addNewItem() async {
+    final String key = _randomValue();
+    final String value = _randomValue();
+
+    await _storage.write(key: key, value: value);
+    _readAll();
+  }
+
+
+  //Generate random strings for key and value
+  String _randomValue() {
+    final rand = Random();
+    final codeUnits = List.generate(20, (index) {
+      return rand.nextInt(26) + 65;
+    });
+
+    return String.fromCharCodes(codeUnits);
+  }
+
+  Future<Null> _performAction(_ItemActions action, _SecItem item) async {
+    switch (action) {
+      case _ItemActions.delete:
+        await _storage.delete(key: item.key);
+        _readAll();
+
+        break;
+      case _ItemActions.edit:
+        final result = await showDialog<String>(
+            context: context,
+            builder: (context) => _EditItemWidget(item.value));
+        if (result != null) {
+          await _storage.write(key: item.key, value: result);
+          _readAll();
+        }
+        break;
+    }
+  }
+  
+
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text('Secure Storage example'),
+      actions: <Widget>[
+        IconButton(
+            key: Key('add_random'),
+            onPressed: _addNewItem,
+            icon: Icon(Icons.add)),
+        PopupMenuButton<_Actions>(
+            key: Key('popup_menu'),
+            onSelected: (action) {
+              switch (action) {
+                case _Actions.deleteAll:
+                  _deleteAll();
+                  break;
+              }
+            },
+            itemBuilder: (BuildContext context) =>
+                <PopupMenuEntry<_Actions>>[
+                  PopupMenuItem(
+                    key: Key('delete_all'),
+                    value: _Actions.deleteAll,
+                    child: Text('Delete all'),
+                  ),
+                ])
+      ],
+    ),
+    body: ListView.builder(
+      itemCount: _items.length,
+      itemBuilder: (BuildContext context, int index) => ListTile(
+        trailing: PopupMenuButton(
+            key: Key('popup_row_$index'),
+            onSelected: (_ItemActions action) =>
+                _performAction(action, _items[index]),
+            itemBuilder: (BuildContext context) =>
+                <PopupMenuEntry<_ItemActions>>[
+                  PopupMenuItem(
+                    value: _ItemActions.delete,
+                    child: Text(
+                      'Delete',
+                      key: Key('delete_row_$index'),
+                    ),
+                  ),
+                  PopupMenuItem(
+                    value: _ItemActions.edit,
+                    child: Text(
+                      'Edit',
+                      key: Key('edit_row_$index'),
+                    ),
+                  ),
+                ]),
+        title: Text(
+          _items[index].value,
+          key: Key('title_row_$index'),
+        ),
+        subtitle: Text(
+          _items[index].key,
+          key: Key('subtitle_row_$index'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ),
+  );
+}
+
+class _EditItemWidget extends StatelessWidget {
+  _EditItemWidget(String text)
+      : _controller = TextEditingController(text: text);
+
+  final TextEditingController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Edit item'),
+      content: TextField(
+        key: Key('title_field'),
+        controller: _controller,
+        autofocus: true,
+      ),
+      actions: <Widget>[
+        FlatButton(
+            key: Key('cancel'),
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('Cancel')),
+        FlatButton(
+            key: Key('save'),
+            onPressed: () => Navigator.of(context).pop(_controller.text),
+            child: Text('Save')),
+      ],
     );
   }
+}
+
+class _SecItem {
+  _SecItem(this.key, this.value);
+
+  final String key;
+  final String value;
 }
